d529ff75890679d0c91b3e671f22a19c434c4209
Daniel Estevez <daniel.ef@gmail.com>
2023-04-05 23:46:08 -0600

    Testing data size

--- main.cc
+++ main.cc
@@ -1,7 +1,10 @@
+
 #include <iostream>
 #include <fstream>
 #include <filesystem>
+#include "session.hh"
 #include "nlohmann/json.hpp"
+
 
 using json = nlohmann::json;
 
@@ -13,6 +16,9 @@ int main(int argc, char** argv) {
   std::cout << "Foo" << std::endl;
   std::cout << "Path separator: " << std::filesystem::path::preferred_separator << std::endl;
 
+  json data = load_config();
+  std::cout << "Number of items in Data: " << data.size() << std::endl;
+  
   return 0;
 }
 
