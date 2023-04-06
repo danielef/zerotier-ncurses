2d28ba5d8ebcc7101ddfd007af0fac09bf70266b
Daniel Estevez <daniel.ef@gmail.com>
2023-04-05 23:58:07 -0600

    Testing data size

--- main.cc
+++ main.cc
@@ -13,9 +13,9 @@ int main(int argc, char** argv) {
   std::cout << "Foo" << std::endl;
   std::cout << "Path separator: " << std::filesystem::path::preferred_separator << std::endl;
 
-  nlohmann::json data = load_config();
-  std::cout << "Number of items in Data: " << data.size() << std::endl;
-  nlohmann::json tokens = data["token"];
+  nlohmann::json config = load_config();
+  std::cout << "Number of items in Data: " << size_config(config) << std::endl;
+  nlohmann::json tokens = config["token"];
   std::cout << "Number of items in Tokens: " << tokens.size() << std::endl;
 
   return 0;
