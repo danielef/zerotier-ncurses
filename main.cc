6b95c61aa68ad38ffb6651ab0efe583cc1500a18
Daniel Estevez <daniel.ef@gmail.com>
2023-04-07 01:57:40 -0600

    Updating libs

--- main.cc
+++ main.cc
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <filesystem>
 #include "session.hh"
+#include "ux.hh"
 #include "nlohmann/json.hpp"
 
 int main(int argc, char** argv) {
@@ -13,18 +14,25 @@ int main(int argc, char** argv) {
   std::cout << "Foo" << std::endl;
   std::cout << "Path separator: " << std::filesystem::path::preferred_separator << std::endl;
 
-  nlohmann::json config = load_config();
-  std::cout << "Number of items in Data: " << size_config(config) << std::endl;
+  nlohmann::json config = session::load_config();
+  std::cout << "Number of items in Data: " << session::size_config(config) << std::endl;
   nlohmann::json tokens = config["token"];
   std::cout << "Number of items in Tokens: " << tokens.size() << std::endl;
 
-  config = add_token(config, "bar");
+  config = session::add_token(config, "bar");
 
   nlohmann::json tokens2 = config["token"];
   std::cout << "Number of items in Tokens: " << tokens2.size() << std::endl;
 
-  save_config(config);
+  session::save_config(config);
+  
+  // Initialize ncurses
+  initscr();
 
+  ux::token_dialog();
+  
+  // Clean up ncurses
+  endwin();
+            
   return 0;
 }
-
