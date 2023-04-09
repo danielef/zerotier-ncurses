7d4c68af2779bc9df897e4017429d51adcd1c638
Daniel Estevez <daniel.ef@gmail.com>
2023-04-08 23:27:50 -0600

    Adding initial_colors

--- ux.hh
+++ ux.hh
@@ -1,12 +1,17 @@
 #ifndef ZEROTIER_UX_HH_
 #define ZEROTIER_UX_HH_
 
+#include <array>
 #include <cstdio>
 #include <string>
 #include <ncurses.h>
 
 namespace ux {
-  
+
+  std::array<short, 2> initial_colors();
+
+  int draw_tabs();
+
   std::string token_dialog();
 
 }
